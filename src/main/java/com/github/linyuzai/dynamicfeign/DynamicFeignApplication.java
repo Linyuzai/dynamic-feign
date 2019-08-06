ce4f8c4276168448a88125eea1576e976a1ac1e4
thz <t88510930>
2019-08-06 14:48:05 +0800

    添加统一配置outUrl的设置

--- src/main/java/com/github/linyuzai/dynamicfeign/DynamicFeignApplication.java
+++ src/main/java/com/github/linyuzai/dynamicfeign/DynamicFeignApplication.java
@@ -1,13 +1,14 @@
 package com.github.linyuzai.dynamicfeign;
 
+import com.github.linyuzai.dynamicfeign.annotation.EnableDynamicFeignClients;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableDynamicFeignClients
 @SpringBootApplication
 public class DynamicFeignApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DynamicFeignApplication.class, args);
     }
-
 }
