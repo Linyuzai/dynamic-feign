d5abb36ca9ea58811c4820f65f8dc7d7d1b915c0
thz <t88510930>
2019-08-13 18:12:19 +0800

    添加lambda支持

--- src/main/java/com/github/linyuzai/dynamicfeign/mapper/DynamicFeignClientMapper.java
+++ src/main/java/com/github/linyuzai/dynamicfeign/mapper/DynamicFeignClientMapper.java
@@ -1,6 +1,7 @@
 package com.github.linyuzai.dynamicfeign.mapper;
 
 import com.github.linyuzai.dynamicfeign.factory.DynamicFeignClientFactoryBean;
+import com.github.linyuzai.dynamicfeign.lambda.*;
 import com.github.linyuzai.dynamicfeign.targeter.Targeter;
 import feign.Client;
 import feign.Feign;
@@ -474,5 +475,85 @@ public class DynamicFeignClientMapper {
         public void setEntity(ConfigurableFeignClientEntity entity) {
             this.entity = entity;
         }
+    }
+
+    public static boolean addMethodUrl(LambdaNoneHolder lambda, String url) {
+        SerializedLambda serializedLambda = SerializedLambda.resolve(lambda);
+        return addMethodUrl(serializedLambda.getImplClass(), serializedLambda.getImplMethodName(), url);
+    }
+
+    public static boolean addMethodUrl(LambdaNoneVoidHolder lambda, String url) {
+        SerializedLambda serializedLambda = SerializedLambda.resolve(lambda);
+        return addMethodUrl(serializedLambda.getImplClass(), serializedLambda.getImplMethodName(), url);
+    }
+
+    public static boolean addMethodUrl(LambdaMapHolder lambda, String url) {
+        SerializedLambda serializedLambda = SerializedLambda.resolve(lambda);
+        return addMethodUrl(serializedLambda.getImplClass(), serializedLambda.getImplMethodName(), url);
+    }
+
+    public static boolean addMethodUrl(LambdaMapVoidHolder lambda, String url) {
+        SerializedLambda serializedLambda = SerializedLambda.resolve(lambda);
+        return addMethodUrl(serializedLambda.getImplClass(), serializedLambda.getImplMethodName(), url);
+    }
+
+    public static boolean addMethodUrl(LambdaStringHolder lambda, String url) {
+        SerializedLambda serializedLambda = SerializedLambda.resolve(lambda);
+        return addMethodUrl(serializedLambda.getImplClass(), serializedLambda.getImplMethodName(), url);
+    }
+
+    public static boolean addMethodUrl(LambdaStringVoidHolder lambda, String url) {
+        SerializedLambda serializedLambda = SerializedLambda.resolve(lambda);
+        return addMethodUrl(serializedLambda.getImplClass(), serializedLambda.getImplMethodName(), url);
+    }
+
+    public static boolean addMethodUrl(LambdaStringArrayHolder lambda, String url) {
+        SerializedLambda serializedLambda = SerializedLambda.resolve(lambda);
+        return addMethodUrl(serializedLambda.getImplClass(), serializedLambda.getImplMethodName(), url);
+    }
+
+    public static boolean addMethodUrl(LambdaStringArrayVoidHolder lambda, String url) {
+        SerializedLambda serializedLambda = SerializedLambda.resolve(lambda);
+        return addMethodUrl(serializedLambda.getImplClass(), serializedLambda.getImplMethodName(), url);
+    }
+
+    public static <T> boolean removeMethodUrl(LambdaNoneHolder<T, ?> lambda) {
+        SerializedLambda serializedLambda = SerializedLambda.resolve(lambda);
+        return removeMethodUrl(serializedLambda.getImplClass(), serializedLambda.getImplMethodName());
+    }
+
+    public static <T> boolean removeMethodUrl(LambdaNoneVoidHolder<T> lambda) {
+        SerializedLambda serializedLambda = SerializedLambda.resolve(lambda);
+        return removeMethodUrl(serializedLambda.getImplClass(), serializedLambda.getImplMethodName());
+    }
+
+    public static <T> boolean removeMethodUrl(LambdaMapHolder<T, ?> lambda) {
+        SerializedLambda serializedLambda = SerializedLambda.resolve(lambda);
+        return removeMethodUrl(serializedLambda.getImplClass(), serializedLambda.getImplMethodName());
+    }
+
+    public static <T> boolean removeMethodUrl(LambdaMapVoidHolder<T> lambda) {
+        SerializedLambda serializedLambda = SerializedLambda.resolve(lambda);
+        return removeMethodUrl(serializedLambda.getImplClass(), serializedLambda.getImplMethodName());
+    }
+
+    public static <T> boolean removeMethodUrl(LambdaStringHolder<T, ?> lambda) {
+        SerializedLambda serializedLambda = SerializedLambda.resolve(lambda);
+        return removeMethodUrl(serializedLambda.getImplClass(), serializedLambda.getImplMethodName());
+    }
+
+    public static <T> boolean removeMethodUrl(LambdaStringVoidHolder<T> lambda) {
+        SerializedLambda serializedLambda = SerializedLambda.resolve(lambda);
+        return removeMethodUrl(serializedLambda.getImplClass(), serializedLambda.getImplMethodName());
+    }
+
+    public static <T> boolean removeMethodUrl(LambdaStringArrayHolder<T, ?> lambda) {
+        SerializedLambda serializedLambda = SerializedLambda.resolve(lambda);
+        return removeMethodUrl(serializedLambda.getImplClass(), serializedLambda.getImplMethodName());
+    }
+
+    public static <T> boolean removeMethodUrl(LambdaStringArrayVoidHolder<T> lambda) {
+        SerializedLambda serializedLambda = SerializedLambda.resolve(lambda);
+        return removeMethodUrl(serializedLambda.getImplClass(), serializedLambda.getImplMethodName());
     }
 }
